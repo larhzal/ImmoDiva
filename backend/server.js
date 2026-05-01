d298e68ff7c9ebd65d3d3656d23c5639e24ee6b1
imane-FDILI <fdiliimane24@gmail.com>
2026-05-01 20:52:30 +0100

    feat: implémentation de l'API de récupération des annonces (SCRUM-53)

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,30 @@
 require("dotenv").config();
 const express = require("express");
-
+const cors = require('cors');
 const app = express();
-app.use(express.json());
 
+app.use(cors());
+app.use(express.json());
+const supabase = require('./src/config/db');
 // Routes
 const userRoutes = require("./src/modules/users/users.routes");
 app.use("/api/users", userRoutes);
+
+app.get('/api/annonces', async (req, res) => {
+    try {
+
+        const { data, error } = await supabase
+            .from('Apartment')
+            .select('*');
+
+        if (error) throw error;
+        
+        res.json(data);
+    } catch (err) {
+        console.error("Erreur backend:", err.message);
+        res.status(500).json({ error: err.message });
+    }
+});
 
 const PORT = process.env.PORT || 5000;
 
